59ee6f0f96f7b05eb592305cc253c2fd264072d1
Igone Schotman <i.e.schotman@students.uu.nl>
2026-01-12 17:41:37 +0100

    filtering measurments for seattle

--- script_seattle.py
+++ script_seattle.py
@@ -1,4 +1,15 @@
 ## Title
-with open ('precipitation.json', encoding='utf-8') as file :
-    data = file.read()
 
+#Imports
+import json
+
+with open ('precipitation.json', encoding='utf-8') as file :
+    data = json.load(file)
+
+measurements_seattle = []
+for measurement in data :
+    if measurement['station'] == 'GHCND:US1WAKG0038' :
+        print(measurement)
+
+
+   
