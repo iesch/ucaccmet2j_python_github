817451b31858b965c96338e29df56d3100cbc37d
Igone Schotman <i.e.schotman@students.uu.nl>
2026-01-12 19:31:13 +0100

    trying to assign values per month

--- script_seattle.py
+++ script_seattle.py
@@ -11,8 +11,12 @@ for measurement in data :
     if measurement['station'] == 'GHCND:US1WAKG0038' :      #this if-statement checks which measurements are from Seattle
         measurements_seattle.append(measurement)        #adding the Seattle measurements to the list
 
-months = []
+months = {}
+value = []
 for measurement_type in measurements_seattle :
     date = measurement_type['date']
     date_split = date.split('-')
-    if date_split[1] 
+    value = measurement_type['value']
+    if date_split[1] not in months :
+        months[date_split[1]] = True
+          
