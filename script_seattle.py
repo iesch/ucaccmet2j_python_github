f10a0dcc44045c2f80414ea266fb6ae172429a8f
Igone Schotman <i.e.schotman@students.uu.nl>
2026-01-13 15:48:44 +0100

    adding a for loop, doesnt work though

--- script_seattle.py
+++ script_seattle.py
@@ -11,47 +11,46 @@ stations_data = {}
 with open ('stations.csv') as stations_csv :
     stations_csv.readline()
     for line in stations_csv:
-        #stations_strip = stations_data.strip
         stations_split = line.split(',')
         stations_data[stations_split[0]] = {
             'state': stations_split[1],
             'station': stations_split[2]
         }
-print(stations_data)
-    # Filtering for values from Seattle
-measurements_seattle = []       #creating an empty list to store the measurements from Seattle
-for measurement in data :
-    if measurement['station'] == 'GHCND:US1WAKG0038' :      #this if-statement checks which measurements are from Seattle
-        measurements_seattle.append(measurement)        #adding the Seattle measurements to the list
 
-    # Creating a dictionary and a list with the months and the total precipitation per month
-monthly_precipitation = {}     #initializing an empty dictionary to store the months and their values
-for measurement_type in measurements_seattle :
-    date = measurement_type['date']         #selecting the dates in the data
-    date_split = date.split('-')            #splitting the dates up by the dashes
-    values = measurement_type['value']          #selecting the precipitation value 
-    if date_split[1] not in monthly_precipitation :     #selecting only the 2nd value (the month) and checking whether its in the dictionary
-        monthly_precipitation[date_split[1]] = 0
-    monthly_precipitation[date_split[1]] += values      #adding the values to the corresponding key (the month)
+for location in stations_data :
+            # Filtering for values from Seattle
+    measurements_location = []       #creating an empty list to store the measurements from Seattle
+    for measurement in data :
+        if measurement['station'] == stations_split[2] :      #this if-statement checks which measurements are from Seattle
+            measurements_location.append(measurement)        #adding the Seattle measurements to the list
 
-total_monthly_precipitation = []        #initializing a list to store the total monthly precipitation values
-for month in monthly_precipitation :
-    total_monthly_precipitation.append(monthly_precipitation[month])            #adding the values to the list
-total_yearly_precipitation = sum(total_monthly_precipitation)    
+            # Creating a dictionary and a list with the months and the total precipitation per month
+    monthly_precipitation = {}     #initializing an empty dictionary to store the months and their values
+    for measurement_type in measurements_location :
+        date = measurement_type['date']         #selecting the dates in the data
+        date_split = date.split('-')            #splitting the dates up by the dashes
+        values = measurement_type['value']          #selecting the precipitation value 
+        if date_split[1] not in monthly_precipitation :     #selecting only the 2nd value (the month) and checking whether its in the dictionary
+            monthly_precipitation[date_split[1]] = 0
+        monthly_precipitation[date_split[1]] += values      #adding the values to the corresponding key (the month)
 
-relative_monthly_precipitation = []         #initializing a list to store the relative monthly values
-for monthly_value in total_monthly_precipitation :
-    relative_monthly_precipitation.append(monthly_value/total_yearly_precipitation)     #calculating the relative values and adding them to the list
-#print(relative_monthly_precipitation)    
+    total_monthly_precipitation = []        #initializing a list to store the total monthly precipitation values
+    for month in monthly_precipitation :
+        total_monthly_precipitation.append(monthly_precipitation[month])            #adding the values to the list
+    total_yearly_precipitation = sum(total_monthly_precipitation)    
 
-    # Formatting and saving the results in a JSON file
-results_Seattle = {'Seattle' : {        
-        'station': 'GHCND:US1WAKG0038',
-        'state': 'WA',
-        'total_monthly_precipitation': total_monthly_precipitation, 
-        'total_yearly_precipitation' : total_yearly_precipitation,
-        'relative_monthly_precipitation' : relative_monthly_precipitation}
-    }
+    relative_monthly_precipitation = []         #initializing a list to store the relative monthly values
+    for monthly_value in total_monthly_precipitation :
+        relative_monthly_precipitation.append(monthly_value/total_yearly_precipitation)     #calculating the relative values and adding them to the list   
+
+            # Formatting and saving the results in a JSON file
+    results_json = {stations_split[0] : {        
+            'station': stations_split[2],
+            'state': stations_split[1],
+            'total_monthly_precipitation': total_monthly_precipitation, 
+            'total_yearly_precipitation' : total_yearly_precipitation,
+            'relative_monthly_precipitation' : relative_monthly_precipitation}
+            }
 
 with open('results.json', 'a', encoding='utf-8') as results: 
-    json.dump(results_Seattle, results, indent=4)
+    json.dump(results_json, results, indent=4)
